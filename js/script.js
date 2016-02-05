00f6c34728721b8979b000c5cefb4cc5a0336a3b
evwilkin <evan.wilkinson@gmail.com>
2016-02-04 21:29:27 -0800

    saving for deployment

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
+	sweetAlert("Let's make a list!", "Type to add, drag to reorder, X to delete");
+
 	$("#addItem").on("submit", function(e) {
 		e.preventDefault();
 
 		var $newItem = $("input").val();
 		$("input").val('');
 		$("ul").append("<li><span>X  </span>" + $newItem + "</li>");
-
+		sweetAlert(
+			{
+			   title: "Added!",      
+			   timer: 600,   
+			   showConfirmButton: false });
 		
-	});
+			}
+		);
 
 	$("ul").on("click", "li", function() {
 		$(this).remove();
